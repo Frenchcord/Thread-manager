77fb7987805d9da17fa01ae7f34140de6b059026
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-19 19:11:24 -0400

    Update async.py
    fixing bug for no args

--- src/async.py
+++ src/async.py
@@ -1,12 +1,15 @@
 import asyncio
 from threading import Thread
 def async_thread(func, args = None):
-  def he(arg):
+  def he(arg = None):
+    if arg is None:
+      asyncio.run(func)
+      return
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(func(arg))
     loop.close()
-  if args is None: 
+  if args is None:
     thread = Thread(target=he)
   else:
     thread = Thread(target=he, args=args)
