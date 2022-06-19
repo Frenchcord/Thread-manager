ae477f20add6c3b5e8d9b694557e2ff53e9d5141
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-19 18:56:29 -0400

    Update async.py

--- src/async.py
+++ src/async.py
@@ -1,9 +1,14 @@
-from asyncio import run
+import asyncio
 from threading import Thread
 def async_thread(func, args = None):
+  def he(arg):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(func(arg))
+    loop.close()
   if args is None: 
-    thread = Thread(target=function)
+    thread = Thread(target=he)
   else:
-    thread = Thread(target=function, args=args)
+    thread = Thread(target=he, args=args)
   thread.start()
   thread.join()
